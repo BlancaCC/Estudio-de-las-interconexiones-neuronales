2c925fa63e51ef145f936130e589b3bd10fc85de
Iker Villegas <ivillegaslabairu@gmail.com>
2022-10-16 13:28:30 +0200

    Código para el cálculo de la entropía correcto

---
+++ test/Formulas.py
@@ -0,0 +1,36 @@
+import numpy as np
+import math
+
+def probabilidad(X):
+
+    X_cases, counts = np.unique(X, return_counts = True, axis = 0)
+    probs = np.zeros(len(X))
+    for i in range(len(X)):
+        for j in range(len(X_cases)):
+            if np.array_equal(X[i],X_cases[j]):
+                probs[i] = counts[j]/len(X)
+    return probs
+
+def palabras(X,bits):
+
+    '''
+    X es un vector con una sucesión de 1's y 0's, y bits es la longitud en la que se va a dividir dicho vector para formar las 
+    palabras. Esta función devuelve un vector con las palabras (a modo de subvectores de unos y ceros) en las que se ha dividido el
+    vector X.
+    '''
+
+    number_index = len(X)/bits
+    return np.array_split(X,number_index)
+
+def entropia(X,bits):
+
+    '''
+    Cálculo de la entropía de un conjunto de datos, donde X es el vector que contiene las palabras y bits la variable correspondiente a
+    la longitud de dichas palabras
+    '''
+    X_words = palabras(X,bits)
+    probs = probabilidad(X_words)
+    H = 0
+    for p in probs:
+        H += -p*math.log(p,2)
+    return H
---
+++ test/Formulas.utils.py
@@ -0,0 +1,13 @@
+import Formulas as f
+import numpy as np
+
+def test_entropia():
+    X = np.array([0,1,0,0,0,1,1,0])
+    expect_result = 2
+    bits = 2
+    assert f.entropia(X,bits) == expect_result, "Incorrect output"
+
+
+if __name__ == '__main__':
+    test_entropia()
+    print('All test passed')
