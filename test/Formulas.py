9ee72501bc82df4d87cdb5b7e639add7b06a56b4
Iker Villegas <ivillegaslabairu@gmail.com>
2022-10-18 16:16:49 +0200

    Mejora de la función words() donde se ha añadido el parámetro stride para el deslizamiento de ventana

--- test/Formulas.py
+++ test/Formulas.py
@@ -1,8 +1,7 @@
 import numpy as np
 import math
 
-def probabilidad(X):
-
+def probability(X):
     X_cases, counts = np.unique(X, return_counts = True, axis = 0)
     probs = np.zeros(len(X))
     for i in range(len(X)):
@@ -11,26 +10,34 @@ def probabilidad(X):
                 probs[i] = counts[j]/len(X)
     return probs
 
-def palabras(X,bits):
-
+def words(X,bits,stride):
     '''
     X es un vector con una sucesión de 1's y 0's, y bits es la longitud en la que se va a dividir dicho vector para formar las 
     palabras. Esta función devuelve un vector con las palabras (a modo de subvectores de unos y ceros) en las que se ha dividido el
     vector X.
     '''
+    list = []
+    for i in range(0,len(X)-stride,stride):
+        list.append(X[i:(i+bits)])
+    X_words = np.array(l)
+    print(X_words)
+    return(X_words)
 
-    number_index = len(X)/bits
-    return np.array_split(X,number_index)
-
-def entropia(X,bits):
+def entropy(X,bits,stride):
 
     '''
     Cálculo de la entropía de un conjunto de datos, donde X es el vector que contiene las palabras y bits la variable correspondiente a
     la longitud de dichas palabras      
     '''
-    X_words = palabras(X,bits)
-    probs = probabilidad(X_words)
+    X_words = words(X,bits,stride)
+    probs = probability(X_words)
     H = 0
     for p in probs:
         H += -p*math.log(p,2)
     return H
+
+def join_probability(X,Y,bits):
+    probs_x = probability(X)
+    probs_y = probability(Y)
+    
+
--- test/Formulas.utils.py
+++ test/Formulas.utils.py
@@ -1,13 +1,14 @@
 import Formulas as f
 import numpy as np
 
-def test_entropia():
+def test_entropy():
     X = np.array([0,1,0,0,0,1,1,0])
     expect_result = 2
-    bits = 2
-    assert f.entropia(X,bits) == expect_result, "Incorrect output "
+    bits = 3
+    stride = 4
+    assert f.entropy(X,bits,stride) == expect_result, "Incorrect output "
 
 
 if __name__ == '__main__':
-    test_entropia()
+    test_entropy()
     print('All test passed')
