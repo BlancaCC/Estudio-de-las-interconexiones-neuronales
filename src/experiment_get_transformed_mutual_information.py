f5c4d841d157308221d618fe776463d0118b62be
Blanca <blancacano2.71828@gmail.com>
2022-10-27 00:11:34 +0200

    Añado ficheros con las tablas de IM con nuevos tamaños de ventana #24 estos se encuentran en experiment_results/entropia_mutua_normalizada/entropia_mutua_normalizada_*.txt

--- src/experiment_get_transformed_mutual_information.py
+++ src/experiment_get_transformed_mutual_information.py
@@ -1,27 +1,28 @@
-""
+""" 
+"""
 import sys
 
-from signal_to_binary import transformed_binary_signal  
+from signal_to_binary import binary_signal  
 from constants import  NEURONS
 from formulas import mutual_information
 
-binary_signal = transformed_binary_signal
+
 # Global variables
 bits = list(range(1,9))
 
 
 ## Read signal 
 if(len(sys.argv) == 1):
-    print ('| trozo | bits | stride | IM(LP,VD) |   ')
-    print('| :-: | :-: | :-: | :-: | ')
+    print ('| trozo | window | bits | stride | IM(LP,VD) |   ')
+    print('| :-: | :-: | :-: | :-: | :-: |  ')
     for trozo in "C R G".split():
         for b in bits:
             stride = [b]
             for s in stride:
                 if s <= b:
                     MI = mutual_information(
-                        binary_signal[trozo][NEURONS[0]],
-                        binary_signal[trozo][NEURONS[1]],
+                        transformed_binary_signal[trozo][NEURONS[0]],
+                        transformed_binary_signal[trozo][NEURONS[1]],
                         b,
                         s
                         )
--- src/experiment_get_transformed_normalized_mutual_information.py
+++ src/experiment_get_transformed_normalized_mutual_information.py
@@ -3,19 +3,22 @@ Muestra una tabla con las distintas Informaciones mutuas normalizadas
 """
 import sys
 
-from signal_to_binary import transformed_binary_signal
-from constants import NEURONS
+from signal_to_binary import binary_signal
+from constants import NEURONS, NUMBER_OF_SAMPLES
 from formulas import normalized_mutual_information
 
-binary_signal = transformed_binary_signal
 # Global variables
 bits = list(range(1, 9))
 
-
+window_sizes = {
+    'C' : [11,87, 187],
+    'R' : [17,42, 125],
+    'G' : [66,93, 248]
+}
 # Si no se introducen argumentos al llamar el programa calcula la información mutua
 # para todos los ficheros
-print('| trozo | bits | stride | IM(LP,VD) | E(LP -> VD)| E( VD -> LP) |  ')
-print('| :-: | :-: | :-: | :-: | :-: | :-: | ')
+print('| trozo | window size  | bits | stride | IM(LP,VD) | E(LP -> VD)| E( VD -> LP) |  ')
+print('| :-: | :-: |  :-: | :-: | :-: | :-: | :-: | ')
 
 if (len(sys.argv) == 1):
     trozos = "C R G".split()
@@ -23,16 +26,25 @@ else:
     trozos =  sys.argv[1:]
 
 for trozo in trozos:
-    for b in bits:
-        stride = [b]
-        for s in stride:
-            if s <= b:
-                MI, E_XY, E_YX  = normalized_mutual_information(
-                    binary_signal[trozo][NEURONS[0]],
-                    binary_signal[trozo][NEURONS[1]],
-                    b,
-                    s
-                )
-                print(f' | {trozo}| {b} | {s} | {MI} | {E_XY} | {E_YX} |  ')
+    for w in window_sizes[trozo]:
+        n = NUMBER_OF_SAMPLES[trozo] //w
+        transformed_binary_signal = {}
+        for neuron in NEURONS:
+            transformed_binary_signal[neuron] = [ 
+                max( binary_signal[trozo][neuron][i*w : (i+1)*w]) 
+                for i in range(0, n)
+            ]
+
+        for b in bits:
+            stride = [b]
+            for s in stride:
+                if s <= b:
+                    MI, E_XY, E_YX  = normalized_mutual_information(
+                        transformed_binary_signal[NEURONS[0]],
+                        transformed_binary_signal[NEURONS[1]],
+                        b,
+                        s
+                    )
+                print(f' | {trozo}| {w} | {b} | {s} | {MI} | {E_XY} | {E_YX} |  ')
 
  
