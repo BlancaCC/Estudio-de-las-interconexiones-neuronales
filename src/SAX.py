d5fe9f4160e40c9f5910c62abc748952677327b3
Blanca <blancacano2.71828@gmail.com>
2022-11-01 19:51:27 +0100

    Incluyo implementaciónde SAX #25 Falta calcular el test para varios tamaños de alfabeto

--- src/SAX.py
+++ src/SAX.py
@@ -1,6 +1,7 @@
 """
 Implementación de las funciones relacionadas con el algoritmo de codificación SAX
 """
+import numpy as np
 
 def PAA ( C : list[float], w:int) -> list[float]:
     ''' Realiza la transformación Piecewise Aggregation Approximation
@@ -20,22 +21,70 @@ def PAA ( C : list[float], w:int) -> list[float]:
     ]
     return bar_C
 
+def normalize(X):
+    '''Normaliza según una distribución normal
+    '''
+    sigma = np.std(X)
+    mu = np.mean(X)
+    return (X - mu)/sigma
+
+def between_index(n:float, l:list[float], alphabet_size:int)-> int:
+    '''
+    Devuelve el índice del número anterior para el que es menor
+    '''
+    cnt = 0
+    while cnt < alphabet_size and n > l[cnt]:
+        cnt += 1
+    return cnt
+
 # Blanca:  Me he quedado por aquí implementando
-def PAA_to_SAX(bar_C, a):
+def PAA_to_SAX(bar_C:list[float], alphabet_size:int):
     """
     Realiza paso 2 de discretización
-    """
+    Argumentos: 
+    `bar_C`: Serie temporal 
+    `alphabet_size`: tamaño del alfabeto de la nueva serie.
+    ``
     # Pasos a realizar 
     # 1. Normalización de bar_C
-    # 2. Sacar percentiles en función de a
+    # 2. Sacar percentiles en función de `alphabet_size` y suponiendo normalidad.
     # 3. Calcular alfabeto(lista de número no de caracteres pa poder utilizar nuestro algoritmos de MI)
     # 4. Asignar a cada bar_C el *caracter/entero* que le corresponda 
-
+    """
+    # 1. Normalización de bar_C
+    normalized = normalize([bar_C])[0]
+    # 2 Cálculo de percentiles
+    X = np.random.normal(0,1,100)
+    step = 100/alphabet_size
+    percentiles = [
+        np.percentile(X, i*step)
+        for i in range(alphabet_size)
+    ]
+    new_signal = list(
+        map(lambda x: between_index(x,percentiles,alphabet_size ),
+        normalized
+        )
+    )
+    return new_signal
+    
 
 ### test 
 
-if __name__ == '__main__':
+if True: 
+#__name__ == '__main__':
     C = [1,2,1,0, 0, 0 ]
     w =  3
     print(PAA(C,w))
+    # test: between_index
+    a = 3
+    l = [0,1,2]
+    for i in [-0.1,0.1,1.1,2.2]:
+        print(f' para {i} -> {between_index(i,l,3)}')
 
+    # Test: PAA_to_SAX
+    s = [1,1,1,1,2,2,3,10,-10,2,2,2,2,1,1,1,1,0,0,3,0,3]
+    print(s)
+    for a in range(1,10, 3):
+        print(f'''{a}:
+        {PAA_to_SAX(s, a)}
+''')
